6a80333778c107a6dfd4745687f0830ff4b2b9c2
Serdar <serdar.onculer.work@hotmail.com>
2022-12-13 00:00:01 +0300

    fixed mobile-app.comp

--- src/components/mobile-app/mobile-app.component.jsx
+++ src/components/mobile-app/mobile-app.component.jsx
@@ -5,12 +5,12 @@ import { ReactComponent as AppGalleryLogo } from "assets/images/app-gallery.svg"
 
 const MobileApp = () => {
   return (
-    <div className="md:container md:mx-auto">
+    <div className="md:container md:mx-auto overflow-hidden">
       <div className="bg-primary-color bg-mobile-app-background md:rounded-lg items-center text-white flex flex-row justify-between">
-        <div className="flex flex-col gap-y-3 p-10" style={{maxWidth:"400px"}}>
+        <div className="flex flex-col gap-y-3 p-10" >
           <h3 className="text-[1.625rem] tracking-tight font-bold ">Getir'i indirin!</h3>
           <p className="font-semibold">İstediğiniz ürünleri dakikalar içinde kapınıza <br /> getirelim.</p>
-          <nav className="mt-5 flex gap-x-2">
+          <nav className="mt-5 flex flex-col md:flex-row gap-2">
             <a href="#" className="transform hover:scale-105 transition">
               <AppStoreLogo />
             </a>
@@ -22,8 +22,8 @@ const MobileApp = () => {
             </a>
           </nav>
         </div>
-        <picture className="self-end ">
-          <img src="https://cdn.getir.com/getirweb-images/common/landing/phoneLanding.png" alt="" />
+        <picture>
+          <img className="hidden md:block max-w-[605px] max-h-[288px] ml-40 xl:ml-0" src="https://cdn.getir.com/getirweb-images/common/landing/phoneLanding.png" alt="" />
         </picture>
       </div>
     </div>
